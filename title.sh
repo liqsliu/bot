fae20a9b768aaa44e4c39d79b329330545c833cf
liqsliu <liqsliu@gmail.com>
2025-02-11 18:05:46 +0800

    20250211_180546: commit by liqsliu/wtfipfs.eu.org/GNU/Linux/Linux/x86_64

--- title.sh
+++ title.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # MAX_SHARE_FILE_SIZE=${MAX_SHARE_FILE_SIZE:-64000000}
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+UA="curl/7.88.1"
 LA='Accept-Language: zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6'
 
 # https://stackoverflow.com/questions/20317945/limit-size-wget-can-download/20318140#20318140
@@ -12,27 +13,36 @@ export https_proxy="http://127.0.0.1:6080"
 # https://stackoverflow.com/questions/55842311/get-page-titles-from-a-list-of-urls
 # while read -r URL; do
     # echo -n "$URL --> "
-    URL=$1
+  URL=$1
   if [[ "$2" == raw ]]; then
-    wget -T $MAX_TIMEOUT -q -O - "$URL" | \
-      tr "\n" " " | \
-      sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||' || echo "E: $?"
+    html=$(wget -T $MAX_TIMEOUT -q -O - "$URL") || {
+      wget --server-response -T $MAX_TIMEOUT -O - "$URL" || echo "E: $?"
+      exit
+    }
+
   elif [[ "$2" == direct ]]; then
     unset http_proxy
     unset https_proxy
-    wget -T $MAX_TIMEOUT -q -O - "$URL" | \
-      tr "\n" " " | \
-      sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||' || echo "E: $?"
+    html=$(wget -T $MAX_TIMEOUT -q -O - "$URL") || {
+      wget --server-response -T $MAX_TIMEOUT -O - "$URL" || echo "E: $?"
+      exit
+    }
+
   elif [[ "$2" == curl ]]; then
-    curl -L -m $MAX_TIMEOUT -s -o - -H "$LA" "$URL" -A "$UA" \
-      tr "\n" " " | \
-      sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||' || echo "E: $?"
+    html=$(curl -s -L -m $MAX_TIMEOUT -o - -H "$LA" "$URL" -A "$UA") || {
+      curl -v -L -m $MAX_TIMEOUT -o - -H "$LA" "$URL" -A "$UA" || echo "E: $?"
+      exit
+    }
+
   else
-    wget --header="$LA" --user-agent="$UA" -T $MAX_TIMEOUT -q -O - "$URL" | \
-      tr "\n" " " | \
-      sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||' || echo "E: $?"
+    html=$(wget --user-agent="$UA" --header="$LA" --header="Accept: */*" -T $MAX_TIMEOUT -q -O - "$URL") || {
+      wget --server-response --user-agent="$UA" --header="$LA" --header="Accept: */*" -T $MAX_TIMEOUT -O - "$URL" || echo "E: $?"
+      exit
+    }
   fi
-    echo
+  echo "$html" | tr "\n" " " | \
+  sed 's|.*<title>\([^<]*\).*</head>.*|\1|;s|^\s*||;s|\s*$||' || echo "E: $?"
+  echo
 # done
 
 
