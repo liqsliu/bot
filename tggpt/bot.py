d5a20e2bbd4d80a632f0452a60f07ba91b241572
liqsliu <liqsliu@gmail.com>
2025-02-19 13:05:29 +0800

    20250219_130529: commit by u0_a128/localhost/Android/Linux/aarch64

--- tggpt/bot.py
+++ tggpt/bot.py
@@ -1583,7 +1583,7 @@ async def _init_myshell():
   #  t2 = asyncio.create_task(myshell_p.stderr.readline())
   t1 = asyncio.create_task(pr(p.stdout.readline, 1))
   t2 = asyncio.create_task(pr(p.stderr.readline, 2))
-  cmds = ["source", "~/.bash_profile"]
+  cmds = "source ~/.bash_profile"
   res = await myshell(cmds)
   info(f"init bash: {res}")
   #  info("clean...")
