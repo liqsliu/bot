ab28f9064017db42a4f62bf63297314aefda75f7
liqsliu <liqsliu@gmail.com>
2025-02-23 20:11:44 +0800

    20250223_201144: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -4907,6 +4907,8 @@ async def msgt(event):
       if len(l[2]) > 0:
     #  if chat_id == 5968721572:
         await sleep(0.5)
+      if chat_id == 5968721572:
+        await sleep(0.5)
     if parse_message_deleted_task is not None:
       if not parse_message_deleted_task.done():
         info(f"等待处理完tg的消息删除事件 {parse_message_deleted_task}")
