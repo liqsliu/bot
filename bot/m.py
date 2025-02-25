feaa9cfd25ca3eea494478146a9bdcac9245be2f
liqsliu <liqsliu@gmail.com>
2025-02-25 11:30:34 +0800

    20250225_113034: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -8149,7 +8149,7 @@ async def _run_cmd(text, src, name="X test: ", is_admin=False, textq=None):
           mtmsgs = mtmsgsg[src]
           mtmsgs[pid] = [name]
 
-          if pid in bridge:
+          if pid in bridges:
             bridges.pop(chat_id)
           if pid not in bridges_tmp:
             bridges_tmp[pid] = src
