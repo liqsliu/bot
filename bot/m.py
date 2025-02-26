7ddbd31766e4cf67aef56c90348048cbcd416a0d
liqsliu <liqsliu@gmail.com>
2025-02-26 11:55:50 +0800

    20250226_115550: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -4031,6 +4031,7 @@ def pvb_init2(server=None):
   global pvb_args, pvb_client, pvb_CONFIG
   if server is not None:
     pvb_CONFIG["server"] = server
+  pvb_client = PrivateBin(pvb_CONFIG)
 
 #  def sendpv(text):
 @exceptions_handler
@@ -4042,7 +4043,6 @@ async def pvb(text, server=None):
   try:
     orig = sys.stdout
     sys.stdout = tmp_for_pvb_print
-    pvb_client = PrivateBin(pvb_CONFIG)
     pbincli.actions.send(pvb_args, pvb_client, settings=pvb_CONFIG)
     #  args.func(args, api_client, settings=CONFIG)
   except PBinCLIException as pe:
