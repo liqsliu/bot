6950e3c2e47710c32d85096a4ba10051723a1a4a
liqsliu <liqsliu@gmail.com>
2025-03-01 15:34:41 +0800

    20250301_153441: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -5972,7 +5972,11 @@ async def run_run(coro, need_main=False):
   def cb_for_fu_result(fu):
     #  oloop.call_soon_threadsafe(partial(f, f2()))
     #  oloop.call_soon_threadsafe(partial(fua.set_result, fu.result()))
-    fua.set_result(fu.result())
+    #  fua.set_result(fu.result())
+    res = fu.result()
+    info(f"fu.result: {res}")
+    fua.set_result(res)
+    info(f"done")
   fu.add_done_callback(cb_for_fu_result)
   return await fua
 
