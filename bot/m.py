72bab1485c47654ede226019545cca9679d85df3
liqsliu <liqsliu@gmail.com>
2025-03-01 15:52:14 +0800

    20250301_155214: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -5977,7 +5977,7 @@ async def run_run(coro, need_main=False):
     res = fu.result()
     info(f"fu.result: {res}")
     #  fua.set_result(res)
-    loop.call_soon_threadsafe(fua.set_result, res)
+    oloop.call_soon_threadsafe(fua.set_result, res)
     #  oloop.call_soon(fua.set_result, res)
     info(f"done")
   fu.add_done_callback(cb_for_fu_result)
