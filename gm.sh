e881bb5b8f06a6235cbaf9c8b8c7fa187be2d43b
liqsliu <liqsliu@gmail.com>
2025-03-08 21:36:19 +0800

    20250308_213619: commit by liqsliu/wtfipfs.eu.org/GNU/Linux/Linux/x86_64

--- gm.sh
+++ gm.sh
@@ -2,8 +2,8 @@
 export SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd )}
 res=$(curl -m 1 -s http://127.0.0.1:4241/api/messages) || exit 0
 if [[ "$res" != "[]" ]]; then
-date >> ~/mt.log
-echo "gm.sh log: $res" >> ~/mt.log
+# date >> ~/mt.log
+# echo "gm.sh log: $res" >> ~/mt.log
 res=$(echo "$res" | jq 'del(.[].Extra.file[0].Data)') &>/dev/null || exit 0
 bash "$SH_PATH/gm2.sh" "$res"
 fi
