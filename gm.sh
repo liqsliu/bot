9cce7f7514d58d5e9cadac169a2cbfae410b778f
liqsliu <liqsliu@gmail.com>
2025-03-08 19:59:38 +0800

    20250308_195938: commit by u0_a128/localhost/Android/Linux/aarch64

--- gm.sh
+++ gm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd )}
-res=$(curl -m 2 -s http://127.0.0.1:4241/api/messages) || exit 0
+res=$(curl -m 1 -s http://127.0.0.1:4241/api/messages) || exit 0
 if [[ "$res" != "[]" ]]; then
 res=$(echo "$res" | jq 'del(.[].Extra.file[0].Data)') &>/dev/null || exit 0
 bash "$SH_PATH/gm2.sh" "$res"
