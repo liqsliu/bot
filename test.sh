6de8a5af18a01629e66f8a5e047701efb23bc98d
liqsliu <liqsliu@gmail.com>
2025-03-09 20:33:56 +0800

    20250309_203356: commit by liqsliu/wtfipfs.eu.org/GNU/Linux/Linux/x86_64

---
+++ test.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+export SH_PATH=${SH_PATH:-$(cd $(dirname ${BASH_SOURCE[0]}) || exit; pwd )}
+export LOG_FILE=${LOG_FILE:-/dev/null}
+export LOG=${LOG:-$HOME/mt.log}
+get_msg(){
+  local username=$1
+  local text=$2
+  # local msg
+  if [[ -z "$URL" || "$URL" == "null" ]]; then
+    if [[ -z "$text" ]]; then
+      return 1
+      # continue
+    else
+      bash "$SH_PATH/change_long_text.sh" "$username$text"
+    fi
+  else
+    if [[ -n "$gateway" ]]; then
+      Comment=$(echo "$restmp" | jq -r ".Extra.file[0].Comment")
+      if [[ -z "$Comment" ]]; then
+        bash "$SH_PATH/change_long_text.sh" "$username$text $URL"
+      else
+        bash "$SH_PATH/change_long_text.sh" "$username$text$Comment: $URL"
+      fi
+    fi
+  fi
+  # echo "$msg"
+}
+
+
+parse_msg(){
+  local restmp=$res
+  echo "start to check restmp: $restmp" &>> $LOG_FILE
+  TEXT=$(echo "$restmp" | jq -r ".text")
+#    echo "$text" | sed '/^[^>]/,$d'
+#    text=$(echo "$text" | sed '/^[^>]/,$!d')
+  NAME=$(echo "$restmp" | jq -r ".username")
+#    username="$(echo "$text" | head -n1 | cut -s -d ":" -f1 ): "
+#    text=$(echo "$text" | cut -d" " -f3-)
+#    text="${text:${#username}}"
+  gateway=$(echo "$restmp" | jq -r ".gateway")
+  username=$(echo "$NAME" | tail -n1)
+
+  text=$TEXT
+  [[ "${username:0:2}" != "C " ]] && [[ "${username: -5}" != "bot: " ]] && {
+    QT=$(echo "$NAME" | sed -e '/^> [^>]/!d')
+    qt=$(echo "$QT" | sed -e 's/^> //')
+    if [[ -n "$qt" ]]; then
+      text="$TEXT
+
+$qt"
+    else
+      text="$TEXT"
+    fi
+  }
+  if [[ "$gateway" == "gateway1" ]]; then
+    URL=$(echo "$restmp" | jq -r ".Extra.file[0].URL")
+    get_msg "**${username% }** " "$text"
+    # msg=$(get_msg "**${username% }** " "$text")
+    # if [[ "$username" != "O bot: " ]]; then
+    # if [[ "${username:0:2}" != "O " ]]; then
+    # account=$(echo "$restmp" | jq -r ".account")
+    # # if [[ "${username:0:2}" != "T " ]]; then
+    # if [[ "${account}" != "api.tox" ]]; then
+    #   # msg=$(get_msg "$username" "$text") || continue
+    #   echo "send to tox: $msg" &>> $LOG_FILE
+    #   echo "$msg"
+    # fi
+    # if [[ "${username:0:2}" != "Q " ]]; then
+    #   bash "$SH_PATH/mqtt.sh" "$msg"
+    # fi
+  elif [[ "$gateway" == "test" ]]; then
+    # echo 2 > "$SH_PATH/.BUSY"
+    if [[ -e "$SH_PATH/.BUSY" ]]; then
+      rm "$SH_PATH/.BUSY"
+    fi
+  fi
+}
+
+my_cmd(){
+  export LOG="$HOME/mt.log"
+  [[ -e "$SH_PATH/DEBUG" ]] && export LOG_FILE=$LOG || export LOG_FILE=/dev/null
+  local e=0
+  [[ -e "$SH_PATH/.ERROR" ]] && rm "$SH_PATH/.ERROR"
+  local out=$("$@" 2> "$SH_PATH/.ERROR") || e=$?
+  [[ -f "$SH_PATH/.ERROR" ]] && [[ -n "$(cat "$SH_PATH/.ERROR")" ]] && {
+    echo "res=$res" &>> $LOG
+    if [[ -e "$SH_PATH/DEBUG" ]]; then
+      local d=$(bash -x "$@" 2>&1) || e=$?
+      echo "d=$d" &>> $LOG
+      bash "$SH_PATH/sm.sh" "C bot" "$(
+      echo '#DEBUG'
+      echo "$d"
+      echo '#DEBUG'
+      echo "---"
+      echo "E: $e"
+      )" 4240 test &>> $LOG_FILE
+    else
+      bash "$SH_PATH/sm.sh" "C bot" "E: $? $(
+      cat "$SH_PATH/.ERROR"
+      echo "---"
+      echo "$out"
+      )" 4240 &>> $LOG
+    fi
+  }
+  if [[ -n "$out" ]]; then
+    echo "$out"
+  fi
+}
+
+
+
+while read -r line; do
+date
+echo "got: $line"
+# done < `curl -s curl http://localhost:4242/api/stream`
+done <<< `ping -c 9 localhost`
+
+exit
+
+#
+# https://stackoverflow.com/a/36368249
+curl --buffer -N -s http://127.0.0.1:4241/api/stream | while read -r res; do
+res=$(echo "$res" | jq 'del(.Extra.file[0].Data)') &>/dev/null || continue
+my_cmd parse_msg
+done
+
+exit
+
+res=$(curl -m 1 -s http://127.0.0.1:4241/api/messages) || exit 0
+if [[ "$res" != "[]" ]]; then
+# date >> ~/mt.log
+# echo "gm.sh log: $res" >> ~/mt.log
+res=$(echo "$res" | jq 'del(.[].Extra.file[0].Data)') &>/dev/null || exit 0
+bash "$SH_PATH/gm2.sh" "$res"
+fi
+
