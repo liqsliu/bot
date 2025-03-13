4d809531c1a69d5ceb3b2d3e362050115cb9305f
liqsliu <liqsliu@gmail.com>
2025-03-13 19:35:49 +0800

    20250313_193549: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -3038,8 +3038,8 @@ def send_log(text, jid=None, delay=1, fm=None):
       await sleep(delay*m)
     else:
       info(f"send_log tg: {text}")
-    asyncio.create_task(send_tg(text, CHAT_ID, name="send_log")
-  if jid is None or isinstance(jid, int) is False:
+    asyncio.create_task(send_tg(text, CHAT_ID, name="send_log"))
+    if jid is None or isinstance(jid, int) is False:
     if n > 0:
       warn(f"send_log xmpp is busy: {n} text: {short(text)}")
       await sleep(delay*n)
