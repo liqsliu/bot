fdeeb241bd1df30b1689d271e619327da1ab63d5
liqsliu <liqsliu@gmail.com>
2025-03-15 12:05:07 +0800

    20250315_120507: commit by /localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -8748,7 +8748,7 @@ async def _run_cmd(text, src, name="X test: ", is_admin=False, textq=None):
       #    info(res, exc_info=e)
       f = exceptions_handler(send_to=src)(cmd_funs[cmd])
       res = await f(cmds, src)
-      info(f"res: {r}")
+      info(f"res: {res}")
       if type(res) is tuple:
         if res[0] == 1 or res[0] == 3:
           bot_name = res[1]
