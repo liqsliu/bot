acf5d4e1c8e1a098aabaa1621c144b351e1019cb
liqsliu <liqsliu@gmail.com>
2025-04-07 20:37:47 +0800

    20250407_203747: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -2399,12 +2399,12 @@ async def run_coro(coro, lp, lp2):
     lp.call_soon_threadsafe(fu.set)
     #  return res
   #  info(f"lp2 is_running: {lp2.is_running()}")
-  #  t = loop2.create_task(f())
+  t = lp2.create_task(f())
   #  ts = []
   #  def f2():
   #    t= asyncio.create_task(f())
   #    ts.append(t)
-  lp2.call_soon_threadsafe(asyncio.create_task, f())
+  #  lp2.call_soon_threadsafe(asyncio.create_task, f())
   #  info(f"lp2 is_running: {lp2.is_running()}")
   await fu.wait()
   return ress[0]
