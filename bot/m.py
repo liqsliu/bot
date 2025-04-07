e8fe6bd64349d29c3e310b489696796bd1631d82
liqsliu <liqsliu@gmail.com>
2025-04-07 14:48:16 +0800

    20250407_144816: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -1684,7 +1684,7 @@ def format_byte(num):
 #    #  if myshell_p.returncode is None:
 #    #    return True
 
-#  @cross_thread(need_main=False)
+@cross_thread(need_main=False)
 async def init_myshell():
   #  if "myshell_p" not in globals():
   info("start my shell...")
@@ -1816,7 +1816,7 @@ async def init_myshell():
 SHELL_CMD_LINE_MAX = 1024
 #  async def myshell(cmd, max_time=interval, src=None):
 @exceptions_handler
-#  @cross_thread(need_main=False)
+@cross_thread(need_main=False)
 async def myshell(cmds, max_time=run_shell_time_max, src=None):
   # 有个问题，不知道何时运行结束，目前想到两种方案：bash -i和最后发送echo end然后等出现end提示。
   #  if await init_myshell():
