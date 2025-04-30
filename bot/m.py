630db91b7292eaa2be91068c7816fe8f60c97391
liqsliu <liqsliu@gmail.com>
2025-04-30 14:39:46 +0800

    20250430_143946: commit by u0_a128/localhost/Android/Linux/aarch64

--- bot/m.py
+++ bot/m.py
@@ -3990,7 +3990,8 @@ async def _send_tg(client, lock, last, chats, text, chat_id=CHAT_ID, correct=Fal
   #      parse_mode = client.parse_mode
   #  info(f"parse_mode: {parse_mode}")
   if urlre.fullmatch(text):
-    text2 = name2 + raw_md(text)
+    #  text2 = name2 + raw_md(text)
+    text2 = "{}[{}]({})".fromat(name2, text, text)
   else:
     text2 = name2 + text
   ts = await split_long_text(text2, MAX_MSG_BYTES_TG, tmp_msg)
